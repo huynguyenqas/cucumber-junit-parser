e6f81e3bd91a61e38cfd40df68ae6f722415b8dd
Phu Ta <tathanhphu@gmail.com>
2018-12-17 16:29:43 +0700

    Update code-snippet.js

--- code-snippet.js
+++ code-snippet.js
@@ -1,13 +1,12 @@
 const { execSync } = require("child_process");
 // read built-in $TESTCASES_AC and build command to run scheduled scenarinos
 // or read content of file path process.env.MAGIC_VARIABLES_FILE_PATH
-let command = '';
 let cucumberOptions = "--junit,--step-notifications ";
 if ($TESTCASES_AC && 0 < $TESTCASES_AC.length) {
     cucumberOptions += "-n '" + $TESTCASES_AC.replace(/,/g, '|') + "'";
 } 
 // build command line
-command=`mvn -Dcucumber.options="${cucumberOptions}" clean test`;
+let command = `mvn -Dcucumber.options="${cucumberOptions}" clean test`;
 
 console.log(`=== executing command ===`);
 
