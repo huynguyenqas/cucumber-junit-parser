b4b252474b5881de900815aac31cfd7af31118f5
Phu Ta <tathanhphu@gmail.com>
2018-12-17 16:08:28 +0700

    Update code-snippet.js

--- code-snippet.js
+++ code-snippet.js
@@ -2,11 +2,11 @@ const { execSync } = require("child_process");
 // read built-in $TESTCASES_AC and build command to run scheduled scenarinos
 // or read content of file path process.env.MAGIC_VARIABLES_FILE_PATH
 let command = 'mvn clean test';
-let cucumberOptions = '';
+let cucumberOptions = "--junit,--step-notifications ";
 if ($TESTCASES_AC && 0 < $TESTCASES_AC.length) {
-	cucumberOptions = $TESTCASES_AC.replace(/,/g, '|');
+    cucumberOptions += "-n '" + $TESTCASES_AC.replace(/,/g, '|') + "'";
     // run scheduled scenarios by scenario name
-    command=`mvn -Dcucumber.options="-n '${cucumberOptions}'" clean test`;
+    command=`mvn -Dcucumber.options="${cucumberOptions}" clean test`;
 } 
 console.log(`=== executing command ===`);
 
